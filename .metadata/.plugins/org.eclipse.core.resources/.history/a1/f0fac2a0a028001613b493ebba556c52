1e3a23048daa1f3033723d8947ebac0cc5a692d7
akayerov <akayerov@yandex.ru>
2016-06-20 14:54:38 +0300

    В работе проверка ОГРН

--- .metadata/.plugins/org.eclipse.core.resources/.history/a1/f0fac2a0a028001613b493ebba556c52
+++
@@ -1,6 +0,0 @@
-package akayerov.fillsnils;
-
-public interface ReaderSnils {
-	public String getNextSnils();
-	public int SetPath(String path);
-}
--- FillSnils/src/main/java/akayerov/fillsnils/Program.java
+++ FillSnils/src/main/java/akayerov/fillsnils/Program.java
@@ -1,10 +1,19 @@
 package akayerov.fillsnils;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
 import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import akayerov.fillsnils.dao.MoDAO;
 import akayerov.fillsnils.dao.SnilsDAO;
+import akayerov.fillsnils.models.Mo;
 import akayerov.fillsnils.models.Snils;
 
 
@@ -12,16 +21,40 @@ import akayerov.fillsnils.models.Snils;
 public class Program {
 
     private static final Logger logger = Logger.getLogger(Program.class);
+	private static final String DIR_COMLETE = "COMPLETE";
+	private static final String DIR_ERROR   = "ERROR";
 
     public static void main(String[] args) {
        if(args.length < 1) {
            logger.error("Usage: java fillsnils <path with snils file>");
            return;
        }
-    	
+       String sDirComlete = args + "\\" + DIR_COMLETE;
+       File dirComlete = new File(sDirComlete);
+       if (dirComlete.exists() && dirComlete.isDirectory()) {
+           logger.info("Обработаные файлы перемещаются в папку:" + sDirComlete);
+       }
+       else {
+           logger.info("Папка не найдена, будет создана сейчас" + sDirComlete);
+           dirComlete.mkdir();
+       }    
+       String sDirError = args + "\\" + DIR_ERROR;
+       File dirError = new File(sDirError);
+       if (dirError.exists() && dirError.isDirectory()) {
+           logger.info("Файлы с ошибками перемещаются в папку:" + sDirError);
+       }
+       else {
+           logger.info("Папка не найдена, будет создана сейчас" + sDirError);
+           dirComlete.mkdir();
+       }    
+
+       
+       
     	ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/beans.xml");
 		SnilsDAO snils = (SnilsDAO) context.getBean("storageSnils");
-    	ReaderSnils rd = (ReaderSnils) context.getBean("readerSnils"); 
+		MoDAO mo = (MoDAO) context.getBean("storageMo");
+  
+		ReaderSnils rd = (ReaderSnils) context.getBean("readerSnils"); 
     	FolderSnils folder = (FolderSnils) context.getBean("folderSnils"); 
     	
     	folder.SetPath(args[0]);
@@ -29,8 +62,16 @@ public class Program {
         
     	while(fileNameObj != null) {
     		if(!fileNameObj.ogrn.equals("999999999")) {
-		     	rd.SetPath(fileNameObj.fullpath);
-		        logger.info("Список СНИЛС из файла:");
+	            // проверка корректности ОГРН
+ 	  		  Mo m = mo.getByOgrn(fileNameObj.ogrn);
+	          if( m == null) {
+	             logger.error("ОГРН отсуствует в списке МО:" + fileNameObj.ogrn);
+	          } 
+	          else
+              logger.info("ОГРН:" + fileNameObj.ogrn + "найден. Далее обработка.");
+    			
+    			rd.SetPath(fileNameObj.fullpath);
+		        logger.info("Список СНИЛС из файла:" + fileNameObj.fullpath);
 		        String s;
 		
 		        s = rd.getNextSnils();
@@ -45,12 +86,16 @@ public class Program {
 		          } 
 		          else
 		             logger.info("СНИЛС уже есть базе:" + s);
+		             
 		          s = rd.getNextSnils();
 		        }
     		}
     		else {
-   	           logger.error("Error Name file:" + fileNameObj.fullpath);
+   	           logger.error("Error Name File:" + fileNameObj.fullpath);
+   	           Move(fileNameObj.fullpath,sDirError);
+   	           
     		}
+    		Move(fileNameObj.fullpath,sDirComlete);
 	        fileNameObj  = folder.getNextFile();
     	}      	
         
@@ -61,6 +106,17 @@ public class Program {
 			logger.info(p);
 		}
     }
+
+	private static void Move(String fullpath, String sDirDistination) {
+		// TODO Auto-generated method stub
+	       Path movefrom = FileSystems.getDefault().getPath(fullpath);
+	       Path target_dir = FileSystems.getDefault().getPath(sDirDistination);
+	        try {
+	            Files.move(movefrom, target_dir.resolve(movefrom.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+	        } catch (IOException e) {
+	            System.err.println(e);
+	        }
+	}
 
 
 
